bfed305cbe8d21f943cbb66ed146d147f3dc98e1
dmitriy.teteruk <dmitriy.teteruk@gmail.com>
2021-11-08 18:41:04 +0300

    added class Side

--- exercise03/Program.cs
+++ exercise03/Program.cs
@@ -21,47 +21,57 @@ namespace exercise03
 			points[0] = GetPointFromUser("A"); // присваиваем значение точки 0 с названием А. 
 			points[1] = GetPointFromUser("B");
 			points[2] = GetPointFromUser("C");
-			//points[3] = GetPointFromUser("D");
-			//points[4] = GetPointFromUser("E");
+			points[3] = GetPointFromUser("D");
+			points[4] = GetPointFromUser("E");
 
 			// расчет сторон
 			// формула вычисления стороны по координатам
 			// d = кв.кор из (x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)
 			double AB = CalcSideLength(points[0], points[1]);
 			Console.WriteLine($"Line length AB = {AB}");
-			// следующие 6 сторон
+			
 			double BC = CalcSideLength(points[1], points[2]);
 			Console.WriteLine($"Line length BC = {BC}");
-			//double CD = CalcSideLength(points[2], points[3]);
-			//double DE = CalcSideLength(points[3], points[4]);
-			//double AE = CalcSideLength(points[0], points[4]);
+			
+			double CD = CalcSideLength(points[2], points[3]);
+			Console.WriteLine($"Line length CD = {CD}");
+
+			double DE = CalcSideLength(points[3], points[4]);
+			Console.WriteLine($"Line length DE = {DE}");
+
+			double AE = CalcSideLength(points[0], points[4]);
+			Console.WriteLine($"Line length AE = {AE}");
+
 			double AC = CalcSideLength(points[0], points[2]);
 			Console.WriteLine($"Line length AC = {AC}");
-			//double CE = CalcSideLength(points[2], points[4]);
-			// следующие 6 сторон
+			
+			double CE = CalcSideLength(points[2], points[4]);
+			Console.WriteLine($"Line length CE = {CE}");
 
+			Side[] sides = new sides[3];
 
 			// площадь каждого треугольника
-			//double ABC = CalcTriangleArea(AB, BC, AC);
-			// следюущие 2 треугольника
-			//double ACE = CalcTriangleArea(AC, CE, AE);
-			//double ECD = CalcTriangleArea(CE, CD, DE);
+			double ABC = CalcTriangleArea(AB, BC, AC);
+			double ACE = CalcTriangleArea(AC, CE, AE);
+			double ECD = CalcTriangleArea(CE, CD, DE);
 
-			//общая площадь
-			//double totalArea = ABC + ACE + ECD;
+			//общая площадь пятиугольника
+			double totalArea = ABC + ACE + ECD;
 
 			// вывод на консоль consolewriteln 
 
 		}
 		// функция 01 расчета площади треугльника 
-		//static double CalcTriangleArea(double side1, double side2, double side3)
-		//{
+		static double CalcTriangleArea(double side1, double side2, double side3) // для использования side - создали класс Side			    
+		{
 			// расчет площади треугольника по длине сторон
 			// формула Герона		 S = √ (a + b + c)/2 * ((a + b + c)/2 − a) * ((a + b + c)/2 − b) * ((a + b + c)/2 − c)
 			// где 	p = (a + b + c)/2 : 2
 			//public double side1 = Math.Sqrt();
 		//	throw new NotImplementedException();
-		//}
+			double CalcTriangleArea = Math.Sqrt(1/2*())
+			return CalcTriangleArea;
+		}
 
 		// функция 02 расчета длины стороны треугольника
 		static double CalcSideLength (Point point1, Point point2) // здесь Point - это класс, а point1 и point2 - значения.
@@ -87,6 +97,14 @@ namespace exercise03
 		{
 			public double X;
 			public double Y;
+		}
+
+		// создаем класс - Side
+		public class Side
+		{
+			public double side1;
+			public double side2;
+			public double side3;
 		}
 	}
 }
